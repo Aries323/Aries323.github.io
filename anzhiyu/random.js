4643a8c2ad454a19a2957f5c2f7b1ffdc6bdd32f
Aries323 <1095306368@qq.com>
2024-04-11 16:44:47 +0800

    Site updated: 2024-04-11 16:44:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/10/hello-world/"];function toRandomPost(){
+var posts=["2024/04/10/hello-world/","2024/04/11/Qsolution/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
