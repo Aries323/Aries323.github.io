d2bfb7911361b2f82473d43dad552a4a922ec2af
Aries323 <1095306368@qq.com>
2024-04-12 11:31:09 +0800

    Site updated: 2024-04-12 11:31:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/12/博客搭建记录/","2024/04/11/Qsolution/","2023/12/19/准费米能级与大注入效应/"];function toRandomPost(){
+var posts=["2024/04/11/Qsolution/","2024/04/12/博客搭建记录/","2023/12/19/准费米能级与大注入效应/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
